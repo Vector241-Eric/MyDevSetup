a24fc5b904fc1aafd747d4e940a1ada92e5955f6
Eric Anderson <eric@vector241.com>
2016-08-31 10:25:41 -0500

    Utility for zip files.

--- Modules/FileUtilities.psm1
+++ Modules/FileUtilities.psm1
@@ -1,3 +1,26 @@
 function Remove-VimTempFiles {
   Get-ChildItem -Recurse -Include "*~" | Remove-Item
 }
+
+function Expand-ZIPFile() {
+  Param(
+    [Parameter(Mandatory=$True)]
+    [string]$File,
+
+    [Parameter()]
+    [string]$Destination
+  )
+
+  $filePath = (Resolve-Path -Path $File).Path
+  if (-not $Destination) {
+    $Destination = Split-Path -Path $filePath -Parent
+  }
+
+  Write-Host "Unzipping [$filePath] to [$Destination]"
+
+  $shell = new-object -com shell.application
+  $zip = $shell.NameSpace($filePath) 
+  foreach($item in $zip.items()) { 
+    $shell.Namespace($Destination).copyhere($item) 
+  } 
+}
